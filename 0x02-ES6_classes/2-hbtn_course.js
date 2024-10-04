bb1cf72b56c35584f381fe65dc40b3425d530063
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-10-04 14:45:30 +0300

    hbtn

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else throw new TypeError('Name must be a string');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    } else throw new TypeError('Length must be a number');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(value) {
+    if (Array.isArray(value)) {
+      this._students = value;
+    } else throw new TypeError('Students must be an array of strings');
+  }
+
+  get students() {
+    return this._students;
+  }
+}

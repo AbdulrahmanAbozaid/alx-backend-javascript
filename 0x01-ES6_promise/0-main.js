edd5481c357656fb91785a60fd820f0f5773f1e9
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-10-05 01:25:07 +0300

    currenct

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -1,4 +1,6 @@
 import handleProfileSignup from './6-final-user';
 
-const queue = await handleProfileSignup('John', 'Doe', 'Gerald.jpg');
-console.log(queue);
+(async () => {
+  const queue = await handleProfileSignup('John', 'Doe', 'Gerald.jpg');
+  console.log(queue);
+})();

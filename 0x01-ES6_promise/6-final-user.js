4324c0badf55e208f4f090a8e4b37882d9e9c8c8
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-10-05 01:21:46 +0300

    currenct

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,8 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUp = signUpUser(firstName, lastName);
   const upload = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUp, upload]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+  return Promise.allSettled([signUp, upload]).then((results) => results.map((result) => (
+    {
+      status: result.status,
+      value:
+        result.status === 'fulfilled' ? result.value : result.reason.message,
+    })));
 }

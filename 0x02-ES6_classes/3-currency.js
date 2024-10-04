908e1d7002bea12ed8242ed18e05724dcc516fd3
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-10-04 14:53:24 +0300

    currenct

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`
+  }
+
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else throw new TypeError('code must be a string');
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else throw new TypeError('name must be a string');
+  }
+}

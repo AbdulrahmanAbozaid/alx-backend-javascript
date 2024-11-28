26729ab9c9ee36a23af500cf3a30e3ea7341c70c
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-11-29 00:09:25 +0200

    Nodejs Basics

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,22 @@
+const readDatabase = require('../utils');
+
+class StudentsController {
+  static async getAllStudents(req, res) {
+    try {
+      const students = await readDatabase(process.argv[2]);
+      res.write('This is the list of our students\n');
+      Object.keys(students).forEach((field) => {
+        res.write(
+          `Number of students in ${field}: ${
+            students[field].length
+          }. List: ${students[field].join(', ')}\n`
+        );
+      });
+      res.end();
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+}
+
+module.exports = StudentsController;

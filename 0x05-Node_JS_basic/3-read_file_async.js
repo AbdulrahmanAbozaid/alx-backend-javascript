3185c95e729bddadfa69e481158a64c8613da684
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-11-29 00:19:08 +0200

    Nodejs Basics

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -23,8 +23,8 @@ function countStudents(path) {
       for (const [field, names] of Object.entries(fields)) {
         console.log(
           `Number of students in ${field}: ${names.length}. List: ${names.join(
-            ', '
-          )}`
+            ', ',
+          )}`,
         );
       }
 

32f7666ae6131d50ee7fbd2a801c8501b3a91725
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-12-04 00:25:26 +0200

    Node Basics 5-http

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+
+const fs = require('fs');
 
 const app = express();
 
@@ -8,12 +9,41 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.write('This is the list of our students\n');
-  countStudents(process.argv[2])
-    .then(() => res.end())
-    .catch((err) => {
-      res.end(err.message);
+
+  const path = process.argv.length > 2 ? process.argv[2] : 'njn.csv';
+
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      res.write('Cannot load the database');
+      res.end();
+      return;
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1); // Remove the header line
+
+    res.write(`Number of students: ${students.length}\n`);
+
+    const fields = {};
+    students.forEach((student) => {
+      const [firstname, , , field] = student.split(',');
+      if (!fields[field]) fields[field] = [];
+      fields[field].push(firstname);
     });
+
+    for (const [field, names] of Object.entries(fields)) {
+      res.write(
+        `Number of students in ${field}: ${names.length}. List: ${names.join(
+          ', ',
+        )}${field === 'SWE' ? '' : '\n'}`,
+      );
+    }
+
+    res.end();
+  });
 });
 
 app.listen(1245);
